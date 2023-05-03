8d61893a7ba8f5674b3041358ff0c78e706d0f2e
zitraxmode <bobrenskix.bogdan@gmail.com>
2023-05-03 10:33:16 +0300

    add salary and people package

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from salary import calculate_salary
-from people import get_employees
+from salary_package.salary import calculate_salary
+from people_package.people import get_employees
 from datetime import datetime
 
 if __name__ == '__main__':
